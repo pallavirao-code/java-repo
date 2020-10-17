9ee0edb7e36d2a4a95cf9211fda3d145abf2269f
pallavirao-code <72134541+pallavirao-code@users.noreply.github.com>
2020-10-17 12:49:43 +0530

    Day2 Assigment

---
+++ Employee1/src/com/employee1/Employee.java
@@ -0,0 +1,16 @@
+package com.employee1;
+
+public class Employee {
+
+    String name;
+    int age;
+    String city;
+
+    public  void Display()
+    {
+        System.out.println("The name is"+" "+name);
+        System.out.println("The age is"+" "+age);
+        System.out.println("The city is"+" "+city);
+        System.out.println();
+    }
+}
---
+++ Employee1/src/com/employee1/Main.java
@@ -0,0 +1,21 @@
+package com.employee1;
+
+public class Main {
+
+    public static void main(String[] args) {
+	 Employee emp1=new Employee();
+	 Employee emp2=new Employee();
+
+	 emp1.name="Pallavi";
+	 emp1.age=20;
+	 emp1.city="Udupi";
+
+	 emp2.name="Saurab";
+	 emp2.age=23;
+	 emp2.city="chennai";
+
+	 emp1.Display();
+	 emp2.Display();
+
+    }
+}
